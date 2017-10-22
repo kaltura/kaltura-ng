b424cf1329dc05a06ba8045f0cde8ddeca5f206e
amirch1 <amir.chervinsky@kaltura.com>
2017-10-22 11:07:31 +0300

    restore resizehandling directives

--- kaltura-primeng-ui/src/kaltura-primeng-ui.module.ts
+++ kaltura-primeng-ui/src/kaltura-primeng-ui.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputTextModule, MenuModule } from 'primeng/primeng';
 import { StickyDatatableHeaderDirective } from './directives/sticky-datatable-header.directive';
+import { DropdownCloseOnScroll } from './directives/dropdown-close-on-scroll';
+import { MenuCloseOnScroll } from './directives/menu-close-on-scroll';
 import { KalturaCommonModule } from '@kaltura-ng/kaltura-common';
 
 /**
@@ -12,10 +14,14 @@ import { KalturaCommonModule } from '@kaltura-ng/kaltura-common';
         CommonModule, InputTextModule, MenuModule, KalturaCommonModule
     ],
     declarations: <any[]>[
-	    StickyDatatableHeaderDirective
+	    StickyDatatableHeaderDirective,
+        DropdownCloseOnScroll,
+        MenuCloseOnScroll
     ],
     exports: <any[]>[
-	    StickyDatatableHeaderDirective
+	    StickyDatatableHeaderDirective,
+        DropdownCloseOnScroll,
+        MenuCloseOnScroll
     ],
     providers: <any[]>[
     ]
