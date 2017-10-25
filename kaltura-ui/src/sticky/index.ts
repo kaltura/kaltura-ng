590a504f2028c1f9ed48afd46761a9791b11d27d
amirch1 <amir.chervinsky@kaltura.com>
2017-10-25 13:38:09 +0300

    refactor: move scroll-to-top component to kmc-ng project

--- kaltura-ui/src/sticky/index.ts
+++ kaltura-ui/src/sticky/index.ts
@@ -1,5 +1,4 @@
 export * from './components/sticky.component';
-export * from './components/scroll-to-top.component';
 export * from './directives/sticky.directive';
 export * from './services/sticky-scroll.service';
 export { StickyModule } from './sticky.module';
