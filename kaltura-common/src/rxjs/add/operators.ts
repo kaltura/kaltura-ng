fa9a9fb73c8935738bad33bf9e9e0e3d15f2d26c
benstov <benstov@gmail.com>
2017-11-08 10:37:27 +0200

    feat: add tag rxjs operator
    * allow tagging rxjs operator to track its' status
    * update area blocker to cover popups as well

--- kaltura-common/src/rxjs/add/operators.ts
+++ kaltura-common/src/rxjs/add/operators.ts
@@ -1,9 +1,11 @@
 import { Observable } from 'rxjs/Observable';
 import { _cancelOnDestroy as cancelOnDestroy } from '../operator/cancel-on-destroy/cancel-on-destroy';
 import { _monitor as monitor } from '../operator/monitor/monitor';
+import { _tag as tag } from '../operator/tag/tag';
 
 Observable.prototype.cancelOnDestroy = cancelOnDestroy;
 Observable.prototype.monitor = monitor;
+Observable.prototype.tag = tag;
 
 declare module 'rxjs/Observable' {
     interface Observable<T> {
@@ -11,6 +13,9 @@ declare module 'rxjs/Observable' {
     }
     interface Observable<T> {
         cancelOnDestroy: typeof cancelOnDestroy;
+    }
+    interface Observable<T> {
+        tag: typeof tag;
     }
 }
 
