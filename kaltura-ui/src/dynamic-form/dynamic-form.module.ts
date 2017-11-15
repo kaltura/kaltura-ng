d4e398f5e0861db89ea4b2656b264d506086b0a9
ben.simantov <ben.simantov@kaltura.com>
2017-11-15 17:56:12 +0200

    Added tooltips over custom data fields

--- kaltura-ui/src/dynamic-form/dynamic-form.module.ts
+++ kaltura-ui/src/dynamic-form/dynamic-form.module.ts
@@ -3,12 +3,14 @@ import { DynamicFormService } from './dynamic-form.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DynamicFormItem } from './dynamic-form-item.component';
+import {TooltipModule} from "../tooltip/k-tooltip.module";
 
 @NgModule(
     {
         imports : [
             ReactiveFormsModule,
-            CommonModule
+            CommonModule,
+            TooltipModule
         ],
         declarations : [
             DynamicFormItem
