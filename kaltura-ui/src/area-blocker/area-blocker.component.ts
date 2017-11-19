8ece98b6ade20d36cfdde01f144bc79832fd9aa2
Omri Katz <omri.katz@kaltura.com>
2017-11-19 13:05:56 +0200

    KMS-15911 add visible class indicating the area blocker state; this will allow users to have extra CSS logic based on the area blocker state

--- kaltura-ui/src/area-blocker/area-blocker.component.ts
+++ kaltura-ui/src/area-blocker/area-blocker.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewEncapsulation } from '@angular/core';
 import { AreaBlockerMessage } from './area-blocker-message';
 
 
@@ -14,6 +14,8 @@ export class AreaBlockerComponent implements OnInit  {
 
   public _message : AreaBlockerMessage;
 
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+
   @Input() showLoader : boolean;
   @Input() centerOnScreen : boolean = false;
   @Input() spinnerMarginTop : number = 0;
@@ -22,17 +24,28 @@ export class AreaBlockerComponent implements OnInit  {
   @Input()
   set message(value : AreaBlockerMessage | string)
   {
-    if (typeof value === 'string')
-    {
-      this._message = { title : 'Error', message : value, buttons : [{ label :'Dismiss', action : () => { this._message = null;}}]};
-    }else if (value instanceof AreaBlockerMessage)
-    {
+    if (typeof value === 'string') {
+      this._message = this.stringToMessage(value);
+      this.renderer.addClass(this.elementRef.nativeElement, "visible");
+    } else if (value instanceof AreaBlockerMessage) {
+      this.renderer.addClass(this.elementRef.nativeElement, "visible");
       this._message = value;
-    }else
-    {
+    } else {
+        this.renderer.removeClass(this.elementRef.nativeElement, "visible");
       this._message = null;
     }
   }
+
+  private stringToMessage = (value : string) => new AreaBlockerMessage({
+      title : 'Error',
+      message : value,
+      buttons : [
+          {
+            label :'Dismiss',
+            action : () => this._message = null
+          }
+      ]
+  });
 
   public handleAction(button : { action : () => void}) {
     if (button) {
