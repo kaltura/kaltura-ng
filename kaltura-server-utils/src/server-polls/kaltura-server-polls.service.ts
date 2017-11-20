100870c18c0a04635fc17814bc2ceaa818cb5118
Stas <staskgt@gmail.com>
2017-11-20 09:39:33 +0200

    chore: remove redundant comment

--- kaltura-server-utils/src/server-polls/kaltura-server-polls.service.ts
+++ kaltura-server-utils/src/server-polls/kaltura-server-polls.service.ts
@@ -4,27 +4,6 @@ import { KalturaClient } from '@kaltura-ng/kaltura-client';
 import { Observable } from 'rxjs/Observable';
 import { ServerPolls } from '@kaltura-ng/kaltura-common';
 
-// export class BlaRequestPoll extends KalturaRequest<BaseEntryGetAction> {
-//
-// }
-//
-// // applciation
-// export class bla {
-//
-//   ngOnInit() {
-//     this.ServerPolls.register(10, new BlaRequestPoll())
-//       .cancelOnDestroy(this)
-//       .subscribe(response => {
-//         if (response.error) {
-//
-//         } else if (response.result instanceof BaseEntryGetActionResult) {
-//
-//         }
-//       })
-//   }
-// }
-//
-// kaltura-server-utils
 @Injectable()
 export class KalturaServerPolls extends ServerPolls<KalturaRequest<any>, KalturaAPIException> implements OnDestroy {
   constructor(private _kalturaClient: KalturaClient) {
