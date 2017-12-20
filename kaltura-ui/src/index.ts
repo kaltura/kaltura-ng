a900963fe90d99da35e758fa0a1fa91219f4c004
Dvir Hazout <dvir.hazut@kaltura.com>
2017-12-20 20:23:28 +0700

    feat: input helper popover in dynamic form controls (#36)

--- kaltura-ui/src/index.ts
+++ kaltura-ui/src/index.ts
@@ -1,5 +1,6 @@
 export { KalturaUIModule } from './kaltura-ui.module';
 export { TooltipModule } from './tooltip/k-tooltip.module';
+export { InputHelperModule } from './input-helper';
 export { FileDialogComponent } from './components/file-dialog/file-dialog.component';
 export { KalturaPlayerComponent } from './player/player.component';
 export { KalturaBrowserUtils, BrowserNames } from './kaltura-browser-utils';
