4a8a6621cd78b0cc26303e44f460af1cf6d4b64c
Eran Sakal <eran@sakalim.com>
2017-12-27 16:23:30 +0200

    code adjustments

--- kaltura-ui/src/tooltip/k-tooltip.directive.ts
+++ kaltura-ui/src/tooltip/k-tooltip.directive.ts
@@ -5,118 +5,169 @@ export type TooltipPositions = 'top' | 'left' | 'right' | 'bottom';
 @Directive({
 	selector: '[kTooltip]'
 })
-export class KTooltipDirective implements OnDestroy{
+export class KTooltipDirective implements OnDestroy {
 
-	constructor(private elementRef: ElementRef) {
-	}
-
-	private _tooltip: any = null;
-	private _value: number | string = null;
-
-	@Input() escape: boolean = true;
-	@Input() tooltipOffset: number = 8;
-	@Input() placement: TooltipPositions = "top";
-	@Input() delay = 0;
-	@Input() maxWidth: number = 0;
-	@Input() followTarget = false;
-  
-  @Input() set kTooltip(value: number | string) {
-  	if (typeof value === 'undefined' || value === '' || value === null) {
-  		this._value = null;
-      this.removeTooltip();
-      return;
-		}
-
-  	if (!this._tooltip) {
-      this.createElem(value);
-		} else {
-      this.updateElem(value);
-		}
-  
-    this._value = value;
-	}
-
-	@HostListener("mouseenter") onMouseEnter() {
-		if (this._value !== null && typeof this._value !== 'undefined' && this._value !== '') {
-			document.body.appendChild(this._tooltip || this.createElem(this._value));
-			this.setPosition();
-		}
-	}
-
-	@HostListener("mouseleave") removeTooltip() {
-		if (this._tooltip && this._tooltip.parentNode) {
-			this._tooltip.parentNode.removeChild(this._tooltip);
-			this._tooltip = null;
-		}
-	}
-  
-  @HostListener("mousemove") moveTooltip() {
-    if (this.followTarget && this._tooltip) {
-      this.setPosition();
+    constructor(private elementRef: ElementRef) {
     }
-  }
-  
-  updateElem(value: string | number) {
-    if (this.escape) {
-      this._tooltip.innerHTM = '';
-      this._tooltip.textContent = value;
-    } else {
-      this._tooltip.innerHTML = value;
+
+    private _tooltipElement: HTMLElement = null;
+    private _tooltipText: string = null;
+    private _shouldBeVisible = false;
+    private _tooltipHeight: number = null;
+    private _tooltipWidth: number = null;
+
+
+    @Input() escape: boolean = true;
+    @Input() tooltipOffset: number = 8;
+    @Input() placement: TooltipPositions = "top";
+    @Input() delay = 0;
+    @Input() maxWidth: number = 0;
+    @Input() followTarget = false;
+
+    @Input()
+    set kTooltip(value: string) {
+        if (typeof value === 'undefined' || value === '' || value === null) {
+            this._tooltipText = null;
+        }
+        else {
+            this._tooltipText = value;
+        }
+
+        this._updateTooltipElementContent();
     }
-  }
 
-	createElem(value) {
-		this._tooltip = document.createElement('span');
-		this._tooltip.className += "ng-tooltip ng-tooltip-" + this.placement;
-		if (this.maxWidth > 0){
-			this._tooltip.style.maxWidth = this.maxWidth + "px";
-		}
-    this.updateElem(value);
+    private _updateTooltipElement(): void {
+        if (!this._tooltipText) {
+            if (this._isShowingTooltip()) {
+                this._removeTooltipElement();
+            }
+        } else if (this._shouldBeVisible) {
+            if (!this._isShowingTooltip()) {
+                this._tooltipElement = document.createElement('span');
+                this._tooltipElement.style.pointerEvents = 'none';
+                this._tooltipElement.className += "ng-tooltip ng-tooltip-" + this.placement;
+                if (this.maxWidth > 0) {
+                    this._tooltipElement.style.maxWidth = this.maxWidth + "px";
+                }
 
-		setTimeout(() => {
-			if (this._tooltip) {
-				this._tooltip.className += " ng-tooltip-show";
-			}
-		}, this.delay);
+                const addTooltipVisibleClass = () => {
+                    if (this._tooltipElement) {
+                        this._tooltipElement.className += " ng-tooltip-show";
+                    }
+                };
 
-		return this._tooltip;
+                if (this.delay) {
+                    setTimeout(addTooltipVisibleClass, this.delay);
+                } else {
+                    addTooltipVisibleClass();
+                }
+
+                document.body.appendChild(this._tooltipElement);
+                this._updateTooltipElementContent();
+                this._updateTooltipElementPosition();
+
+            } else {
+                this._updateTooltipElementContent();
+                this._updateTooltipElementPosition();
+            }
+        } else {
+            if (this._isShowingTooltip()) {
+                this._removeTooltipElement();
+            }
+        }
+    }
+
+    @HostListener("mousemove")
+    onMouseMove() {
+        if (this.followTarget) {
+            this._updateTooltipElementPosition();
+        }
+    }
+
+    @HostListener("mouseenter")
+    onMouseEnter() {
+    	this._shouldBeVisible = true;
+    	this._updateTooltipElement();
+    }
+
+    @HostListener("mouseleave")
+    onMouseLeave() {
+        this._shouldBeVisible = false;
+        this._updateTooltipElement();
+    }
+
+    private _removeTooltipElement() {
+        if (this._tooltipElement) {
+            if (this._tooltipElement.parentNode) {
+                this._tooltipElement.parentNode.removeChild(this._tooltipElement);
+            }
+            this._tooltipElement = null;
+            this._tooltipHeight = null;
+            this._tooltipWidth = null;
+        }
+    }
+
+    private _isShowingTooltip(): boolean {
+        return this._tooltipElement !== null;
 	}
 
-	setPosition() {
+    private _updateTooltipElementContent() {
+        if (this._isShowingTooltip()) {
+            if (this.escape) {
+                this._tooltipElement.innerHTML = '';
+                this._tooltipElement.textContent = this._tooltipText;
+            } else {
+                this._tooltipElement.innerHTML = this._tooltipText;
+                this._tooltipElement.textContent = '';
+            }
 
-		const elemPosition = this.elementRef.nativeElement.getBoundingClientRect();
+            this._tooltipHeight = this._tooltipElement.clientHeight;
+            this._tooltipWidth = this._tooltipElement.offsetWidth;
+        } else {
+            this._updateTooltipElement();
+        }
+    }
 
-		let elemHeight = this.elementRef.nativeElement.offsetHeight;
-		let elemWidth = this.elementRef.nativeElement.offsetWidth;
-		let tooltipHeight = this._tooltip.clientHeight;
-		let tooltipWidth = this._tooltip.offsetWidth;
+    private _updateTooltipElementPosition() {
 
-		if (this.placement == 'top') {
-			this._tooltip.style.top = elemPosition.top - (tooltipHeight + this.tooltipOffset) + 'px';
+        if (this._isShowingTooltip()) {
+            const elemPosition = this.elementRef.nativeElement.getBoundingClientRect();
+
+            if (elemPosition) {
+                let elemHeight = this.elementRef.nativeElement.offsetHeight;
+                let elemWidth = this.elementRef.nativeElement.offsetWidth;
+
+                if (this.placement == 'top') {
+                    this._tooltipElement.style.top = elemPosition.top - (this._tooltipHeight + this.tooltipOffset) + 'px';
+                }
+
+                if (this.placement == 'bottom') {
+                    this._tooltipElement.style.top = elemPosition.top + elemHeight + this.tooltipOffset + 'px';
+                }
+
+                if (this.placement == 'top' || this.placement == 'bottom') {
+                    this._tooltipElement.style.left = (elemPosition.left + elemWidth / 2) - this._tooltipWidth / 2 + 'px';
+                }
+
+                if (this.placement == 'left') {
+                    this._tooltipElement.style.left = elemPosition.left - this._tooltipWidth - this.tooltipOffset + 'px';
+                }
+
+                if (this.placement == 'right') {
+                    this._tooltipElement.style.left = elemPosition.left + elemWidth + this.tooltipOffset + 'px';
+                }
+
+                if (this.placement == 'left' || this.placement == 'right') {
+                    this._tooltipElement.style.top = elemPosition.top + elemHeight / 2 - this._tooltipElement.clientHeight / 2 + 'px';
+                }
+            }
+        }else
+		{
+			this._updateTooltipElement();
 		}
+    }
 
-		if (this.placement == 'bottom') {
-			this._tooltip.style.top = elemPosition.top + elemHeight + this.tooltipOffset + 'px';
-		}
-
-		if (this.placement == 'top' || this.placement == 'bottom') {
-			this._tooltip.style.left = (elemPosition.left + elemWidth / 2) - tooltipWidth / 2 + 'px';
-		}
-
-		if (this.placement == 'left') {
-			this._tooltip.style.left = elemPosition.left - tooltipWidth - this.tooltipOffset + 'px';
-		}
-
-		if (this.placement == 'right') {
-			this._tooltip.style.left = elemPosition.left + elemWidth + this.tooltipOffset + 'px';
-		}
-
-		if (this.placement == 'left' || this.placement == 'right') {
-			this._tooltip.style.top = elemPosition.top + elemHeight / 2 - this._tooltip.clientHeight / 2 + 'px';
-		}
-	}
-
-	ngOnDestroy(){
-		this.removeTooltip();
-	}
+    ngOnDestroy() {
+        this._removeTooltipElement();
+    }
 }
