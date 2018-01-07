a7c6953c658d1fd34f72cc414fdc6d4e8f20c33e
Eran Sakal <eran@sakalim.com>
2018-01-07 16:56:53 +0200

    feat: add filter of type Boolean

--- mc-shared/src/filters/filter-types/index.ts
+++ mc-shared/src/filters/filter-types/index.ts
@@ -4,4 +4,5 @@ export * from './list-type';
 export * from './number-type';
 export * from './simple-type-base';
 export * from './string-type';
+export * from './boolean-type';
 export * from './type-adapter-base';
