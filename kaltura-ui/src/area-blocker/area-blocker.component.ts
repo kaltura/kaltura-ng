68a73166f2603b2789772e0fb0f18eb8d2033490
Eran Sakal <eran@sakalim.com>
2018-01-09 09:33:34 +0200

    fix: handle show loader only if needed

--- kaltura-ui/src/area-blocker/area-blocker.component.ts
+++ kaltura-ui/src/area-blocker/area-blocker.component.ts
@@ -11,6 +11,7 @@ export class AreaBlockerComponent implements OnInit  {
 
   private _showLoader: boolean;
   public _message : AreaBlockerMessage;
+  private _showLoaderHandler: number;
 
   @ViewChild('areaBlockerContainer') areaBlockerContainer: ElementRef;
   @ViewChild('spinnerContainer') spinnerContainer: ElementRef;
@@ -19,25 +20,30 @@ export class AreaBlockerComponent implements OnInit  {
   @Input() spinnerMarginTop : number = 0;
   @Input() classes : string;
 
-  @Input() set showLoader(value : boolean){
-    // once showLoader is set to true, use a timeout so *ngIf will cause the HTML to render and then calculate area blocker width
-    if (value){
-      setTimeout(()=>{
-        if (this.bodyScroll && !!this.areaBlockerContainer){
-          const rect = this.areaBlockerContainer.nativeElement.getBoundingClientRect();
-          if (rect.width < document.body.clientWidth && this.spinnerContainer){
-            this._renderer.setStyle(this.spinnerContainer.nativeElement, 'left', `${rect.left + rect.width/2}px`);
+  @Input() set showLoader(value : boolean) {
+      // once showLoader is set to true, use a timeout so *ngIf will cause the HTML to render and then calculate area blocker width
+      if (value) {
+          this._showLoaderHandler = setTimeout(() => {
+              this._showLoaderHandler = null;
+              if (this.bodyScroll && !!this.areaBlockerContainer) {
+                  const rect = this.areaBlockerContainer.nativeElement.getBoundingClientRect();
+                  if (rect.width < document.body.clientWidth && this.spinnerContainer) {
+                      this._renderer.setStyle(this.spinnerContainer.nativeElement, 'left', `${rect.left + rect.width / 2}px`);
+                  }
+              }
+              this._renderer.setStyle(this.spinnerContainer.nativeElement, 'opacity', '1'); // show the spinner only after its position is calculated to prevent seeing it jumps...
+          }, 0);
+      } else {
+          if (this._showLoaderHandler) {
+              clearTimeout(this._showLoaderHandler);
+              this._showLoaderHandler = null;
           }
-        }
-        this._renderer.setStyle(this.spinnerContainer.nativeElement, 'opacity', '1'); // show the spinner only after its position is calculated to prevent seeing it jumps...
-      },0);
-    }else{
-      if (this.bodyScroll && this.spinnerContainer){
-        this._renderer.setStyle(this.spinnerContainer.nativeElement, 'opacity', '0'); // hide the spinner so we won't see it jump next time its position is recalculated
+          if (this.bodyScroll && this.spinnerContainer) {
+              this._renderer.setStyle(this.spinnerContainer.nativeElement, 'opacity', '0'); // hide the spinner so we won't see it jump next time its position is recalculated
+          }
       }
-    }
-    this._showLoader = value;
-  };
+      this._showLoader = value;
+  }
 
   get showLoader(): boolean{
     return this._showLoader;
