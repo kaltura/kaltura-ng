f649672ae0bb04d1294af431f7130f80aaf32439
Eran Sakal <eran@sakalim.com>
2018-01-16 12:06:59 +0200

    feat: add json to xml parser

--- kaltura-common/src/xml-parser/xml-parser.ts
+++ kaltura-common/src/xml-parser/xml-parser.ts
@@ -1,5 +1,53 @@
 import { XmlToJSON } from './xml-to-json';
 
+
+function convertObjectToXml(prefix: string, propertyName: string, propertyValue: any): string {
+    let result = ``;
+
+
+    if (Array.isArray(propertyValue)) {
+        propertyValue.forEach(innerItem =>
+        {
+            result += convertObjectToXml(prefix, propertyName, innerItem);
+        });
+    } else if (typeof propertyValue === 'object') {
+
+        const attributes = propertyValue['attr'];
+        let parsedAttributes = '';
+        if (attributes) {
+            Object.keys(attributes).forEach(attributeName =>
+            {
+                parsedAttributes += `${parsedAttributes ? ' ' : ''}${attributeName}="${attributes[attributeName]}"`;
+            });
+        }
+
+        let parsedValue: any = '';
+
+        if (propertyValue['text']) {
+            parsedValue = propertyValue['text'];
+        } else {
+            Object.keys(propertyValue).forEach(innerProperty => {
+                if (innerProperty !== 'attr') {
+                    parsedValue += convertObjectToXml(prefix, innerProperty, propertyValue[innerProperty]);
+                }
+            });
+        }
+
+        result += `<${prefix}${propertyName}`;
+
+        if (parsedAttributes) {
+            result += ` ${parsedAttributes}>`;
+        } else {
+            result += '>';
+        }
+
+        result += `${parsedValue}</${prefix}${propertyName}>`;
+    }
+
+
+    return result;
+}
+
 export class XmlParser
 {
     static toJson(xml : string) : {}
@@ -14,7 +62,20 @@ export class XmlParser
             });
     }
 
+    static toXml(namespace: string, prefix: string, data: object): string {
 
+        let parsedObject = '';
+        const parsedPrefix = prefix ? `${prefix}:` : '';
+
+        if (data)
+        {
+            Object.keys(data).forEach(property => {
+                parsedObject += convertObjectToXml(parsedPrefix, property, data[property])
+            });
+        }
+
+        return `<${parsedPrefix}schema xmlns:xsd="${namespace}">${parsedObject}</${parsedPrefix}schema>`;
+    }
 
     static toSimpleXml(data : {}, config : {removeEmpty? : boolean} = {}) : string{
         let result = '';
