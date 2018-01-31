cea39914dd3546f79612c219209127df2eff9f17
Roman Olshanskiy <r.olshanskiy@gmail.com>
2018-01-31 18:34:57 +0200

    draggable-data-table - initial commit

---
+++ kaltura-ui/src/draggable-data-table/draggable-data-table.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { DraggableDataTableComponent } from "./draggable-data-table.component";
+import { ColumnComponent } from "./column.component";
+
+
+@NgModule(
+    {
+        imports : [],
+        declarations : [],
+        exports : [
+            DraggableDataTableComponent,
+            ColumnComponent
+        ],
+        providers : []
+    }
+)
+export class DraggableDataTableModule
+{
+
+} 
