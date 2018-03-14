9364af79e57aff8c4f4a5b14570960ed32dc599b
amirch1 <amir.chervinsky@kaltura.com>
2018-03-14 10:45:05 +0200

    refactor: refactor directive to register to scroll only when the dropdown menu is open

--- kaltura-primeng-ui/src/directives/dropdown-close-on-scroll.ts
+++ kaltura-primeng-ui/src/directives/dropdown-close-on-scroll.ts
@@ -1,5 +1,6 @@
 import { Directive, OnDestroy, AfterViewInit, ContentChild, Input } from '@angular/core';
 import { Dropdown } from 'primeng/primeng';
+import { ISubscription } from 'rxjs/Subscription';
 
 @Directive({
 	selector: '[kDropdownCloseOnScroll]',
@@ -9,25 +10,56 @@ export class DropdownCloseOnScroll implements AfterViewInit, OnDestroy {
 	@Input() scrollTarget: any;
 	@ContentChild(Dropdown) public dropdown: Dropdown;
 
+	private _registered = false;
+	private _dropdownChangesSubscription: ISubscription;
+	private _closeDropdownFunc = this.closeDropdown.bind(this);
+	private _isDestroyed = false;
+
 	constructor() {
 	}
 
 	ngAfterViewInit() {
-		if (this.scrollTarget){
-			this.scrollTarget.addEventListener('scroll', this.closeDropdown.bind(this));
+		this.dropdown.el.nativeElement.addEventListener('click', ()=>{
+			this.handleScrollRegistration();
+		});
+		this._dropdownChangesSubscription = this.dropdown.onChange.subscribe((event) => {
+			this.handleScrollRegistration();
+		});
+	}
 
+	handleScrollRegistration():void{
+		if (this.scrollTarget && this.dropdown){
+			setTimeout(()=>{
+				if (!this._isDestroyed) {
+					if (this.dropdown.panelVisible && !this._registered) {
+						this.scrollTarget.addEventListener('scroll', this._closeDropdownFunc);
+						this._registered = true;
+					}
+					if (!this.dropdown.panelVisible && this._registered) {
+						this.scrollTarget.removeEventListener('scroll', this._closeDropdownFunc);
+						this._registered = false;
+					}
+				}
+			},0);
 		}
 	}
 
 	ngOnDestroy() {
-		if (this.scrollTarget) {
-			this.scrollTarget.removeEventListener('scroll', this.closeDropdown.bind(this));
+		if (this.scrollTarget && this._registered) {
+			this.scrollTarget.removeEventListener('scroll', this._closeDropdownFunc);
 		}
+		if (this._dropdownChangesSubscription){
+			this._dropdownChangesSubscription.unsubscribe();
+			this._dropdownChangesSubscription = null;
+		}
+		this._isDestroyed = true;
 	}
 
 	private closeDropdown():void{
 		if (this.dropdown && typeof this.dropdown.hide !== "undefined"){
 			this.dropdown.hide();
+			this.scrollTarget.removeEventListener('scroll', this._closeDropdownFunc);
+			this._registered = false;
 		}
 	}
 }
