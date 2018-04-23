d3a246052c1c8ba6b37dbd6749de2bf368f5e840
Tanya Libatter <Tanya.Libatter@kaltura.com>
2018-04-23 16:31:48 +0300

    add input range control

---
+++ kaltura-ui/src/input-range/input-range.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputRangeComponent } from './input-range.component';
+import { TooltipModule } from "../tooltip/k-tooltip.module";
+
+@NgModule({
+    imports: <any[]>[
+        CommonModule,
+        TooltipModule
+    ],
+    declarations: <any[]>[
+        InputRangeComponent
+    ],
+    exports: <any[]>[
+        InputRangeComponent
+    ],
+    providers: <any[]>[
+    ]
+})
+export class InputRangeModule {}
