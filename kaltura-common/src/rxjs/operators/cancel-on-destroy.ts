89f89a5baf1c8fd68b3d544741c929bc1c1b0f32
Stas <staskgt@gmail.com>
2018-06-14 16:09:23 +0300

    fix codestyle

--- kaltura-common/src/rxjs/operators/cancel-on-destroy.ts
+++ kaltura-common/src/rxjs/operators/cancel-on-destroy.ts
@@ -10,41 +10,41 @@ interface EnhancedOnDestroy extends OnDestroy {
 
 export function cancelOnDestroy<T>(instance: OnDestroy, manualDestroy?: Observable<any>): (source: Observable<T>) => Observable<T> {
     return (source: Observable<T>) => {
-        if (instance.ngOnDestroy) {
-            return Observable.create(observer => {
-                let subscription = source.subscribe(observer);
-                
-                if (!(<EnhancedOnDestroy>instance).__ngOnDestroySource__) {
-                    (<EnhancedOnDestroy>instance).__ngOnDestroySource__ = new Subject();
-                    (<EnhancedOnDestroy>instance).__ngOnDestroy__ = instance.ngOnDestroy;
-                    
-                    instance.ngOnDestroy = function (this: EnhancedOnDestroy) {
-                        this.__ngOnDestroy__.apply(this, arguments);
-                        this.__ngOnDestroySource__.next();
-                        this.__ngOnDestroySource__.complete();
-                    };
-                }
-                
-                const sources = manualDestroy
-                    ? Observable.merge(manualDestroy, (<EnhancedOnDestroy>instance).__ngOnDestroySource__)
-                    : (<EnhancedOnDestroy>instance).__ngOnDestroySource__.asObservable();
-                
-                sources.subscribe(() => {
-                    if (subscription) {
-                        subscription.unsubscribe();
-                        subscription = null;
-                    }
-                });
-                
-                return () => {
-                    if (subscription) {
-                        subscription.unsubscribe();
-                        subscription = null;
-                    }
-                }
-            });
-        } else {
+        if (!instance.ngOnDestroy) {
             return source;
         }
+
+        return Observable.create(observer => {
+            let subscription = source.subscribe(observer);
+            
+            if (!(<EnhancedOnDestroy>instance).__ngOnDestroySource__) {
+                (<EnhancedOnDestroy>instance).__ngOnDestroySource__ = new Subject();
+                (<EnhancedOnDestroy>instance).__ngOnDestroy__ = instance.ngOnDestroy;
+                
+                instance.ngOnDestroy = function (this: EnhancedOnDestroy) {
+                    this.__ngOnDestroy__.apply(this, arguments);
+                    this.__ngOnDestroySource__.next();
+                    this.__ngOnDestroySource__.complete();
+                };
+            }
+            
+            const sources = manualDestroy
+                ? Observable.merge(manualDestroy, (<EnhancedOnDestroy>instance).__ngOnDestroySource__)
+                : (<EnhancedOnDestroy>instance).__ngOnDestroySource__.asObservable();
+            
+            sources.subscribe(() => {
+                if (subscription) {
+                    subscription.unsubscribe();
+                    subscription = null;
+                }
+            });
+            
+            return () => {
+                if (subscription) {
+                    subscription.unsubscribe();
+                    subscription = null;
+                }
+            }
+        });
     }
 }
