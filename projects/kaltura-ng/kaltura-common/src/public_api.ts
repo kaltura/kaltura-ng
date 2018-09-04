cfe744482f58b0d8402a331d96f9b77637757661
amirch1 <amir.chervinsky@kaltura.com>
2018-09-04 14:58:53 +0300

    chore: upgrade Angular CLI, Angular core and PrimeNG libraries

--- projects/kaltura-ng/kaltura-common/src/public_api.ts
+++ projects/kaltura-ng/kaltura-common/src/public_api.ts
@@ -6,7 +6,7 @@ export * from './lib/operation-tag/index';
 export * from './lib/server-polls/index';
 export * from './lib/upload-management/index';
 export * from './lib/utils/index';
-export * from './lib/xml-parser';
+export * from './lib/xml-parser/index';
 export * from './lib/app-storage.service';
 export * from './lib/friendly-hash-id';
 export * from './lib/kaltura-common.module';
