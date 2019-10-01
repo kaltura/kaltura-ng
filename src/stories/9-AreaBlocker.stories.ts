08e29e79d11ed61398cbb0ec493a51a1fbbd6a25
Stas <staskgt@gmail.com>
2019-10-01 15:30:49 +0300

    add areablocker

---
+++ src/stories/9-AreaBlocker.stories.ts
@@ -0,0 +1,45 @@
+import { moduleMetadata, storiesOf } from '@storybook/angular';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AreaBlockerMessage, AreaBlockerModule } from '@kaltura-ng/kaltura-ui';
+import { action } from '@storybook/addon-actions';
+
+storiesOf('AreaBlocker', module)
+  .addDecorator(story => {
+    document.body.classList.add('kOverrideFAIcons');
+    return story();
+  })
+  .addDecorator(
+    moduleMetadata({
+      imports: [
+        CommonModule,
+        BrowserAnimationsModule,
+        AreaBlockerModule,
+      ],
+    })
+  )
+  .add('Default', () => ({
+    template: `
+        <k-area-blocker [showLoader]="true">
+            <div style="width: 100%; height: 20em; background: white"></div>
+        </k-area-blocker>
+    `,
+  }))
+  .add('Message', () => ({
+    template: `
+        <k-area-blocker [message]="message">
+            <div style="width: 100%; height: 20em; background: white"></div>
+        </k-area-blocker>
+    `,
+    props: {
+      message: new AreaBlockerMessage({
+        message: 'Area blocker message content',
+        buttons: [
+          {
+            label: 'Some action button',
+            action: () => action('Some action button event')()
+          }
+        ]
+      })
+    }
+  }));
