cf6a965f1587796f004ff99ee5b7f3aadfbdc502
Tanya Libatter <tanya.libatter@kaltura.com>
2020-03-17 08:54:48 +0200

    Tiered dropdown (#162)
    * add tieredDropdown component
    
    * add tests

--- projects/kaltura-ng/kaltura-primeng-ui/src/lib/kaltura-primeng-ui.module.ts
+++ projects/kaltura-ng/kaltura-primeng-ui/src/lib/kaltura-primeng-ui.module.ts
@@ -1,30 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StickyDatatableHeaderDirective } from './directives/sticky-datatable-header.directive';
-import { DropdownCloseOnScroll } from './directives/dropdown-close-on-scroll';
-import { MenuCloseOnScroll } from './directives/menu-close-on-scroll';
-import { KalturaCommonModule } from '@kaltura-ng/kaltura-common';
-import { InputTextModule } from 'primeng/inputtext';
-import { MenuModule } from 'primeng/menu';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {StickyDatatableHeaderDirective} from './directives/sticky-datatable-header.directive';
+import {DropdownCloseOnScroll} from './directives/dropdown-close-on-scroll';
+import {MenuCloseOnScroll} from './directives/menu-close-on-scroll';
+import {KalturaCommonModule} from '@kaltura-ng/kaltura-common';
+import {InputTextModule} from 'primeng/inputtext';
+import {MenuModule} from 'primeng/menu';
 
 /**
  * @deprecated use separated module for each component
  */
 @NgModule({
-    imports: <any[]>[
-        CommonModule, InputTextModule, MenuModule, KalturaCommonModule
-    ],
-    declarations: <any[]>[
-	    StickyDatatableHeaderDirective,
-        DropdownCloseOnScroll,
-        MenuCloseOnScroll
-    ],
-    exports: <any[]>[
-	    StickyDatatableHeaderDirective,
-        DropdownCloseOnScroll,
-        MenuCloseOnScroll
-    ],
-    providers: <any[]>[
-    ]
+  imports: <any[]>[
+    CommonModule, InputTextModule, MenuModule, KalturaCommonModule
+  ],
+  declarations: <any[]>[
+    StickyDatatableHeaderDirective,
+    DropdownCloseOnScroll,
+    MenuCloseOnScroll
+  ],
+  exports: <any[]>[
+    StickyDatatableHeaderDirective,
+    DropdownCloseOnScroll,
+    MenuCloseOnScroll
+  ],
+  providers: <any[]>[]
 })
-export class KalturaPrimeNgUIModule {}
+export class KalturaPrimeNgUIModule {
+}
