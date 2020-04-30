c2185fc71a2ee86fa2c29d6e0b85b2c706488557
amir.chervinsky <amir.chervinsky@kaltura.com>
2020-04-30 11:00:04 +0300

    remove unused imports

--- projects/kaltura-ng/kaltura-primeng-ui/src/lib/tiered-dropdown/tiered-dropdown.module.ts
+++ projects/kaltura-ng/kaltura-primeng-ui/src/lib/tiered-dropdown/tiered-dropdown.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {TieredMenu, TieredMenuModule} from 'primeng/tieredmenu';
+import { TieredMenuModule} from 'primeng/tieredmenu';
 import {TieredDropdownComponent} from './tiered-dropdown.component';
 import {TieredDropdownSubComponent} from './tiered-dropdown-sub/tiered-dropdown-sub.component';
 import {ButtonModule} from 'primeng/button';
-import {SharedModule} from 'primeng/api/shared';
 
 @NgModule(
   {
